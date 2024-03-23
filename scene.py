947eab28b211910ea9f77c6c36dfb44913c79984
Mehdi_Ba <109386095+MehdiBa13@users.noreply.github.com>
2024-03-23 23:02:56 +0000

    Update scene.py

--- scene.py
+++ scene.py
@@ -1,13 +1,18 @@
 from manim import *
 class GraphTheory(Scene):
 	def construct(self):
-		v = [1, 2, 3, 4, 5]
-		e = [(1, 2), (1, 3), (1, 4), (3, 4), (2, 4), (2, 5), (4, 5)]
-		g = Graph(v, e, labels=True, layout="planar")
-		self.play(Create(g))
+		no1 = "TS"
+		no2 = "BH"
+		no3 = "MD"
+		no4 = "MS"
+		no5 = "HT"
+		v = [no1, no2, no3, no4, no5]
+		e = [(no1, no2), (no2, no3), (no3, no4), (no4, no5), (no5, no1)]
+		g = Graph(v, e, labels=True)
+		self.play(Create(g), run_time=5)
 		self.wait()
-		t1 = Text("What is that?", font="Lucida Console", font_size=38).shift(DOWN)
-		self.play(g.animate.to_edge(UP, buff=0.2))
+		t1 = Text("What is this?", font="Lucida Console", font_size=38).shift(DOWN * 2)
+		self.play(g.animate.to_edge(UP, buff=0.5))
 		self.play(Write(t1))
 		self.play(t1.animate.next_to(g, buff=0.5))
 		t2 = MarkupText(
@@ -26,7 +31,6 @@ class GraphTheory(Scene):
 			font_size=32
 		).next_to(t2, DOWN, buff=0.2)
 		self.play(Write(t2), run_time=2)
-		self.wait()
 		self.play(Write(t2_2), run_time=4)
 		t3 = MarkupText(
 			f"""
@@ -36,22 +40,4 @@ class GraphTheory(Scene):
 			font="Lucida Console",
 			font_size=32
 		).next_to(t2_2, DOWN, buff=0.5)
-		self.wait()
 		self.play(Write(t3), run_time=4)
-		self.play(FadeOut(t1), FadeOut(t2), FadeOut(t2_2), FadeOut(t3))
-		self.play(g.animate.move_to(ORIGIN))
-		te = Text("Graph Terminology", font="Lucida Console").to_edge(UP, buff=0.2)
-		self.play(Write(te))
-		node = MarkupText(f'<span fgcolor="{GREEN}">Node</span>', font='Lucida Console').next_to(g, LEFT, buff=3)
-		edge = MarkupText(f'<span fgcolor="{BLUE}">Edge</span>', font='Lucida Console').next_to(g, DOWN, buff=2)
-		n1p = g.vertices[v.index(1) + 1].get_center()
-		n1l = Text("1").move_to(n1p)
-		a1 = Arrow(node, n1l)
-		self.play(Create(node), Create(a1))
-		self.wait()
-		p1 = g.vertices[1].get_center()
-		p2 = g.vertices[2].get_center()
-		ed = p1 + (p2 - p1) / 2
-		a2 = Arrow(edge, ed)
-		self.play(Create(edge), Create(a2))
-		self.wait()
