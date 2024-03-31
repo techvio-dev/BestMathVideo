48f192fcf658e379904de21ab822440dce91917f
Abdelmonaim Bounite <68242352+techvio-dev@users.noreply.github.com>
2024-03-31 16:47:43 +0000

    Update scene.py

--- scene.py
+++ scene.py
@@ -2,11 +2,11 @@ from manim import *
 import numpy as np
 
 class MyGraph(VGroup):
-	def __init__(self, node_positions, edges, labels, directed=False, graph_shift=np.array([0, 0, 0]), scale_factor=0.5, vertex_radius=0.2, **kwargs):
+	def __init__(self, node_positions, edges, labels=None, directed=False, graph_shift=np.array([0, 0, 0]), scale_factor=0.5, vertex_radius=0.2, **kwargs):
 		super().__init__(**kwargs)
 		self.node_positions = node_positions
 		self.edges = edges
-		self.labels = labels
+		self.labels = labels if labels is not None else {} 
 		self.directed = directed
 		self.graph_shift = graph_shift
 		self.scale_factor = scale_factor
@@ -25,7 +25,7 @@ class MyGraph(VGroup):
 			pos_with_shift = pos * self.scale_factor + self.graph_shift
 			vertex_circle = Circle(radius=self.vertex_radius, color=WHITE, fill_color=dark_blue, fill_opacity=1, stroke_color=WHITE, stroke_width=4).move_to(pos_with_shift)
 			self.vertices.add(vertex_circle)
-			if node in self.labels:
+			if node in self.labels and self.labels[node]:
 				vertex_label = Text(self.labels[node], font_size=24, color=WHITE).move_to(pos_with_shift)
 				self.vertices.add(vertex_label)
 
@@ -34,19 +34,18 @@ class MyGraph(VGroup):
 			start_pos = self.node_positions[start_node] * self.scale_factor + self.graph_shift
 			end_pos = self.node_positions[end_node] * self.scale_factor + self.graph_shift
 			if self.directed:
-				edge_line = Arrow(start=start_pos, end=end_pos, buff=0.1, stroke_width=DEFAULT_STROKE_WIDTH, color=GREY, max_tip_length_to_length_ratio=0.05)  # Adjust arrowhead size if needed
+				edge_line = Arrow(start=start_pos, end=end_pos, buff=0.1, stroke_width=DEFAULT_STROKE_WIDTH, color=GREY, max_tip_length_to_length_ratio=0.05)  # Adjust the arrowhead size if needed
 			else:
 				edge_line = Line(start=start_pos, end=end_pos, stroke_width=DEFAULT_STROKE_WIDTH, color=GREY)
 			self.edge_lines.add(edge_line)
-
 class GraphTheory(Scene):
 	def construct(self):
 		"""
 		axes = Axes(
-			x_range=[-10, 10, 1],
-			y_range=[-6, 6, 1],
-			x_length=config.frame_width - 1,
-			y_length=config.frame_height - 1,
+			x_range=np.array([-10, 10, 1]),
+			y_range=np.array([-6, 6, 1]),
+			x_length=config.frame_width,
+			y_length=config.frame_height,
 			axis_config={"color": BLUE},
 		)
 
@@ -373,7 +372,7 @@ class GraphTheory(Scene):
 		self.play(Write(t78), run_time=1)
 		self.play(Write(t79), run_time=1)
 		self.wait(1)
-		self.play(FadeOut(t76), FadeOut(t77), FadeOut(t78), FadeOut(t79), FadeOut(my_graph), FadeOut(edge_highlight8), FadeOut(edge_highlight9), FadeOut(edge_highlight10), FadeOut(edge_highlight11), run_time=1)
+		self.play(FadeOut(t7), FadeOut(t76), FadeOut(t77), FadeOut(t78), FadeOut(t79), FadeOut(my_graph), FadeOut(edge_highlight8), FadeOut(edge_highlight9), FadeOut(edge_highlight10), FadeOut(edge_highlight11), run_time=1)
 
 
 		node_positions2 = {
@@ -400,8 +399,237 @@ class GraphTheory(Scene):
 
 		shift = np.array([-0.9, 1.3, 0])
 		my_graph3 = MyGraph(node_positions2, edges2, labels2, graph_shift=shift, vertex_radius=0.314)
-		my_graph3.shift(RIGHT)
-		my_graph3.shift(DOWN)
 		self.play(Create(my_graph3.vertices), run_time=2)
 		self.play(Create(my_graph3.edge_lines), run_time=2)
+		self.wait(3)
+		self.play(my_graph3.animate.move_to(np.array([0, 1, 0])).scale(0.75), run_time=1)
+		self.wait(1)
+		t8 = MarkupText(
+			f"""
+			An <span fgcolor="{RED}">Euler Circuit</span> is a cycle that visits every node in the graph
+			""",
+			font="Lucida Console",
+			font_size=25
+		).next_to(my_graph3, DOWN, buff=0.5)
+		t81 = MarkupText(
+			f"""
+			using each edge <span fgcolor="{YELLOW}">exactly once</span>.
+			""",
+			font="Lucida Console",
+			font_size=25
+		).next_to(t8, DOWN, buff=0.2)
+		self.play(Write(t8), run_time=3.5)
+		self.play(Write(t81), run_time=2)
+		self.wait(2)
+		self.play(FadeOut(t8), FadeOut(t81))
+		self.play(my_graph3.animate.move_to(np.array([0, 0, 0])).scale(1.5), run_time=1)
+		self.wait(3)
+		self.play(FadeOut(my_graph3))
+		
+		node_positions3 = {
+			"A": np.array([0*2, 0*2, 0]), 
+			"B": np.array([-1*2, 0*2, 0]), 
+			"C": np.array([1*2, 0*2, 0]), 
+			"D": np.array([2*2, 0*2, 0]),
+			"E": np.array([1*2, 1*2, 0]),
+			"F": np.array([0*2, 1*2, 0]),
+			"G": np.array([0*2, -1*2, 0]),
+			"H": np.array([1*2, -1*2, 0]),
+		}
+		edges3 = [
+			("A", "B"),
+			("A", "G"),
+			("A", "F"),
+			("A", "H"),
+			("B", "F"),
+			("A", "H"),
+			("C", "D"),
+			("C", "E"),
+			("C", "F"),
+			("C", "H"),
+			("D", "H"),
+			("E", "F"),
+			("G", "H"),
+		]
+		labels3 = {
+			"A": "8", 
+			"B": "1", 
+			"C": "4", 
+			"D": "5",
+			"E": "3",
+			"F": "2",
+			"G": "7",
+			"H": "6",
+		}
+
+		# shift = np.array([0, 0, 0])
+		my_graph4 = MyGraph(node_positions3, edges3, labels3, vertex_radius=0.2)
+		self.play(Create(my_graph4.vertices), run_time=2)
+		self.play(Create(my_graph4.edge_lines), run_time=2)
+		self.play(my_graph4.animate.move_to(ORIGIN).scale(1.5), run_time=1)
+		self.wait(1)
+		edge_highlight12 = Line(np.array([-1*2-0.25, 0*2, 0]), np.array([0*2-0.75, 1*2-0.5, 0]), color=RED, stroke_width=10)
+		edge_highlight13 = Line(np.array([0*2-1, 1*2-0.5, 0]), np.array([1*2-1, 1*2-0.5, 0]), color=RED, stroke_width=10)
+		edge_highlight14 = Line(np.array([1*2-1.25, 1*2-0.3, 0]), np.array([1*2-1.25, 0*2+0.3, 0]), color=RED, stroke_width=10)
+		edge_highlight15 = Line(np.array([1*2-1, 0*2, 0]), np.array([2*2-1.75, 0*2, 0]), color=RED, stroke_width=10)
+		edge_highlight16 = Line(np.array([1*2+0.25, 0*2, 0]), np.array([0*2+0.75, -1*2+0.5, 0]), color=RED, stroke_width=10)
+		edge_highlight17 = Line(np.array([0*2+1, -1*2+0.5, 0]), np.array([-1*2+1, -1*2+0.5, 0]), color=RED, stroke_width=10)
+		edge_highlight18 = Line(np.array([-1*2+1.25, -1*2+0.3, 0]), np.array([-1*2+1.25, 0*2-0.3, 0]), color=RED, stroke_width=10)
+		edge_highlight19 = Line(np.array([-0.5*2+0.25, 0*2, 0]), np.array([0*2+0.75, -1*2+0.5, 0]), color=RED, stroke_width=10)
+		edge_highlight20 = Line(np.array([1*2-1.25, -1*2+0.3, 0]), np.array([1*2-1.25, 0*2+0.3, 0]), color=RED, stroke_width=10)
+		edge_highlight21 = Line(np.array([0.5*2-0.25, 0*2, 0]), np.array([0*2-0.75, 1*2-0.5, 0]), color=RED, stroke_width=10)
+		edge_highlight22 = Line(np.array([-1*2+1.25, 1*2-0.3, 0]), np.array([-1*2+1.25, 0*2+0.3, 0]), color=RED, stroke_width=10)
+		edge_highlight23 = Line(np.array([-1*2+1, 0*2, 0]), np.array([-2*2+1.75, 0*2, 0]), color=RED, stroke_width=10)
+
+
+		self.play(Create(edge_highlight12), run_time=0.75)
+		self.play(Create(edge_highlight13), run_time=0.75)
+		self.play(Create(edge_highlight14), run_time=0.75)
+		self.play(Create(edge_highlight15), run_time=0.75)
+		self.play(Create(edge_highlight16), run_time=0.75)
+		self.play(Create(edge_highlight17), run_time=0.75)
+		self.play(Create(edge_highlight18), run_time=0.75)
+		self.play(Create(edge_highlight19), run_time=0.75)
+		self.play(Create(edge_highlight20), run_time=0.75)
+		self.play(Create(edge_highlight21), run_time=0.75)
+		self.play(Create(edge_highlight22), run_time=0.75)
+		self.play(Create(edge_highlight23), run_time=0.75)
+		graphandhighlight = VGroup(my_graph4, edge_highlight12, edge_highlight13, edge_highlight14, edge_highlight15, edge_highlight16, edge_highlight17, edge_highlight18, edge_highlight19, edge_highlight20, edge_highlight21, edge_highlight22, edge_highlight23)
+		self.wait(2)
+		self.play(graphandhighlight.animate.move_to(UP).scale(0.75), run_time=1)
+		t82 = MarkupText(
+			f"""
+			We can say that the edges connected to a nodes can be split into 2 parts, in-edges and out-edges
+			""",
+			font="Lucida Console",
+			font_size=20
+		).next_to(my_graph4, DOWN, buff=0.5)
+		t83 = MarkupText(
+			f"""
+			(for every edge you use to enter the node, or to leave it, we can say that these edges come in pairs).
+			""",
+			font="Lucida Console",
+			font_size=20
+		).next_to(t82, DOWN, buff=0.2)
+		t84 = MarkupText(
+			f"""
+			That means: <span fgcolor="{RED}">Euler Circuit exists if and only if</span> <span fgcolor="{GREEN}">every single node has an even degree.</span>
+			""",
+			font="Lucida Console",
+			font_size=20
+		).next_to(t83, DOWN, buff=0.2).scale(0.75)
+
+		self.play(Write(t82))
+		self.play(Write(t83))
+		self.play(Write(t84))
+		
+		self.wait(1)
+		
+		self.play(FadeOut(graphandhighlight), FadeOut(t82), FadeOut(t83), FadeOut(t84))
+
+		node_positions4 = {
+			"A": np.array([0*2, 1*2, 0]), 
+			"B": np.array([1*2, 0*2, 0]), 
+			"C": np.array([-1*2, 0*2, 0]), 
+			"D": np.array([-1*2, -2*2, 0]),
+			"E": np.array([1*2, -2*2, 0]),
+		}
+		edges4 = [
+			("A", "B"),
+			("A", "C"),
+			("B", "C"),
+			("B", "E"),
+			("C", "D"),
+			("D", "E"),
+			("B", "D"),
+			("C", "E"),
+		]
+		
+		
+		my_graph5 = MyGraph(node_positions4, edges4, vertex_radius=0.1)
+		self.play(Create(my_graph5.vertices), run_time=3)
+		self.play(Create(my_graph5.edge_lines), run_time=3)
+		self.wait(2)
+		self.play(my_graph5.animate.move_to(UP), run_time=1)
+
+		t9 = MarkupText(
+			f"""
+			Can you draw this shape <span fgcolor="{RED}">without taking off the pencil from the paper</span>
+			""",
+			font="Lucida Console",
+			font_size=20
+		).next_to(my_graph5, DOWN, buff=0.5)
+		t91 = MarkupText(
+			f"""
+				and <span fgcolor="{RED}">without going over the same line twice</span>?
+			""",
+			font="Lucida Console",
+			font_size=20
+		).next_to(t9, DOWN, buff=0.2)
+
+		self.play(Write(t9))
+		self.play(Write(t91))
+
+		self.wait(1)
+		
+		self.play(FadeOut(t9), FadeOut(t91))
+		
+		self.wait(1)
+		
+		self.play(my_graph5.animate.move_to(UP), run_time=1)
+
+
+		t10 = MarkupText(
+			f"""
+				Degrees of nodes:
+			""",
+			font="Lucida Console",
+			font_size=20
+		).next_to(my_graph5, DOWN, buff=0.5)
+		t11 = MarkupText(
+			f"""
+				Node 1: 2
+			""",
+			font="Lucida Console",
+			font_size=20
+		).next_to(t10, DOWN, buff=0.25)
+		t12 = MarkupText(
+			f"""
+				Node 2: 4
+			""",
+			font="Lucida Console",
+			font_size=20
+		).next_to(t11, DOWN)
+		t13 = MarkupText(
+			f"""
+				Node 3: 4
+			""",
+			font="Lucida Console",
+			font_size=20
+		).next_to(t12, DOWN)
+		t14 = MarkupText(
+			f"""
+				Node 4: 3
+			""",
+			font="Lucida Console",
+			font_size=20
+		).next_to(t13, DOWN)
+		t15 = MarkupText(
+			f"""
+				Node 5: 3
+			""",
+			font="Lucida Console",
+			font_size=20
+		).next_to(t14, DOWN)
+		self.play(Write(t10), run_time=1)
+		self.play(Write(t11), run_time=1)
+		self.play(Write(t12), run_time=1)
+		self.play(Write(t13), run_time=1)
+		self.play(Write(t14), run_time=1)
+		self.play(Write(t15), run_time=1)
+
+		schoolg = VGroup(my_graph5, t10, t11, t12, t13, t14, t15)
+
+		self.play(schoolg.animate.move_to(LEFT*3), run_time=1)
+
 		self.wait(5)
